7bcf0b4afee2aece7214f66baa7242df4c73e959
tiareWang <a986261752@gmail.com>
2021-02-24 21:50:58 -0800

    1. mybatis使用示例：http://127.0.0.1:9002/system/test

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,14 +1,22 @@
 package com.course.system.controller;
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 //@RequestMapping("/system")   //这样之后的每个地址都加了"/system"
 @RestController
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
     @RequestMapping("/test")   //访问test地址
-    public String test(){
-        return "success";
+    public List<Test> test(){
+        return testService.list();
     }
 }
