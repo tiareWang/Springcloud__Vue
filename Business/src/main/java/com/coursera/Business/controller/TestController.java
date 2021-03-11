6ee364b414edadd0c484285c9e83aeefd9727daf
tiareWang <a986261752@gmail.com>
2021-03-11 15:28:33 -0800

    mm

---
+++ Business/src/main/java/com/coursera/Business/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.coursera.Business.controller;
+
+import com.coursera.server.domain.Test;
+import com.coursera.server.service.TestService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+//@RequestMapping("/system")   //这样之后的每个地址都加了"/system"
+@RestController
+public class TestController {
+
+    @Resource
+    private TestService testService;
+
+    @RequestMapping("/test")   //访问test地址
+    public List<Test> test(){
+        return testService.list();
+    }
+}
