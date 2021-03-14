46bc1c5d9f40ff8abb3835fe4e1cc2910cacb622
tiareWang <a986261752@gmail.com>
2021-03-13 19:14:54 -0800

    1. 通用sidebar点击激活样式方法，使用watch监听路由变化

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -480,8 +480,23 @@
   export default {
     name: 'admin',
     mounted: function() {
+      let _this = this;
       $('body').removeClass('login-layout light-login');
       $('body').attr('class', 'no-skin');
+      //sidebar激活样式方法二
+      _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
+    },
+    watch: {  //用来监视vue上数据的变动（监听）
+      //sidebar激活样式方法二
+      $route: {
+        handler :function(val, oldVal){
+          console.log("--->页面跳转:", val, oldVal);
+          let _this = this;
+          _this.$nextTick(function (){  //页面加载完成后执行
+            _this.activeSidebar(_this.$route.name.replace("/", "-") + "-sidebar");
+          })
+        }
+      }
     },
     methods: {
       /**
