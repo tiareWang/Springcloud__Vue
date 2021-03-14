4d00c40d6da0597809a036d129368f1c898580d2
tiareWang <a986261752@gmail.com>
2021-03-13 16:58:57 -0800

    1. 点击sidebar菜单实现页面跳转并增加激活样式

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -354,16 +354,16 @@
         </div><!-- /.sidebar-shortcuts -->
 
         <ul class="nav nav-list">
-          <li class="">
-            <a href="index.html">
+          <li class="" id="welcome-sidebar">
+            <router-link to="/admin/welcome">
               <i class="menu-icon fa fa-tachometer"></i>
               <span class="menu-text"> 欢迎 </span>
-            </a>
+            </router-link>
 
             <b class="arrow"></b>
           </li>
 
-          <li class="active open">
+          <li class="">
             <a href="#" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
               <span class="menu-text"> 系统管理 </span>
@@ -391,6 +391,30 @@
 
                 <b class="arrow"></b>
               </li>
+            </ul>
+          </li>
+
+          <li class="active open">
+            <a href="#" class="dropdown-toggle">
+              <i class="menu-icon fa fa-list"></i>
+              <span class="menu-text">业务管理 </span>
+
+              <b class="arrow fa fa-angle-down"></b>
+            </a>
+
+            <b class="arrow"></b>
+
+            <ul class="submenu">
+              <li class="active" id="business-chapter-sidebar">
+                <router-link to="/admin/business/chapter">
+                  <i class="menu-icon fa fa-caret-right"></i>
+                  大章管理
+                </router-link>
+
+                <b class="arrow"></b>
+              </li>
+
+
             </ul>
           </li>
 
@@ -458,6 +482,25 @@
     mounted: function() {
       $('body').removeClass('login-layout light-login');
       $('body').attr('class', 'no-skin');
+    },
+    methods: {
+      /**
+       * 菜单激活样式，id是当前点击的菜单id
+       * @param id
+       */
+      activeSidebar: function (id) {
+        //兄弟菜单去掉active样式，自身增加active样式
+        $("#" + id).siblings().removeClass("active");
+        $("#" + id).siblings().find("li").removeClass("active");
+        $("#" + id).addClass("active");
+
+        //如果有父菜单，父菜单的兄弟菜单去掉open active，父菜单增加open active
+        let parentLi = $("#" + id).parents("li");  //sibilings:获取兄弟节点
+        if(parentLi) {
+          parentLi.siblings().removeClass("open active");
+          parentLi.addClass("open active");
+        }
+      }
     }
   }
 </script>
