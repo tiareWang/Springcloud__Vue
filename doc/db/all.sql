1279945527983955c755916632df2e7fcdae98fa
tiareWang <a986261752@gmail.com>
2021-03-16 19:43:06 -0700

    1. 小节表section表结构设计 2. 生成section表持久层代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,3 +1,4 @@
+-- 大章
 drop table if exists `chapter`;
 create table `chapter` (
     `id` char(8) not null comment 'ID',
@@ -17,6 +18,26 @@ insert into `chapter` (id, course_id, name) values ('00000007', '00000000', '测
 insert into `chapter` (id, course_id, name) values ('00000008', '00000000', '测试大章九');
 insert into `chapter` (id, course_id, name) values ('00000009', '00000000', '测试大章十');
 insert into `chapter` (id, course_id, name) values ('00000010', '00000000', '测试大章十一');
+
+-- 小节
+DROP TABLE IF EXISTS `section`;
+CREATE TABLE `section` (
+                           `id` CHAR(8) NOT NULL DEFAULT '' COMMENT 'ID',
+                           `title` VARCHAR(50) NOT NULL COMMENT '标题',
+                           `course_id` CHAR(8) COMMENT '课程|course.id',
+                           `chapter_id` CHAR(8) COMMENT '大章|chapter.id',
+                           `video` VARCHAR(200) COMMENT '视频',
+                           `time` INT COMMENT '时长|单位秒',
+                           `charge` CHAR(1) COMMENT '收费|C 收费；F 免费',
+                           `sort` INT COMMENT '顺序',
+                           `created_at` DATETIME(3) COMMENT '创建时间',
+                           `updated_at` DATETIME(3) COMMENT '修改时间',
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='小节';
+
+INSERT INTO `section` (id, title, course_id, chapter_id, video, time, charge, sort, created_at, updated_at)
+VALUES ('00000001', '测试小节01', '00000001', '00000000', '', 500, 'F', 1, now(), now());
+
 # ------------ 测试
 drop table if exists `test`;
 create table `test` (
