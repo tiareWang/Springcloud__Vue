8113ab6e620787c83ac232584db1c01d4e069006
tiareWang <a986261752@gmail.com>
2021-03-21 16:34:24 -0700

    1. 分类表设计和基本代码生成

---
+++ business/src/main/java/com/coursera/business/controller/admin/CategoryController.java
@@ -0,0 +1,64 @@
+package com.coursera.business.controller.admin;
+
+import com.coursera.server.domain.Category;
+import com.coursera.server.util.ValidatorUtil;
+import com.coursera.server.exception.ValidatorException;
+import com.coursera.server.dto.CategoryDto;
+import com.coursera.server.dto.PageDto;
+import com.coursera.server.dto.ResponseDto;
+import com.coursera.server.service.CategoryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.xml.bind.ValidationException;
+import java.util.List;
+
+@RequestMapping("/admin/category")
+@RestController
+public class CategoryController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryController.class);
+    public static final String BUSINESS_NAME = "分类";
+
+    @Resource
+    private CategoryService categoryService;
+
+    /**
+     * 列表查询
+     */
+    @PostMapping("/list")   //访问category地址
+    public ResponseDto list(@RequestBody PageDto pageDto) {
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.list(pageDto);
+        responseDto.setContent(pageDto);
+        return responseDto;
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    @PostMapping("/save")   //访问category地址
+    public ResponseDto save(@RequestBody CategoryDto categoryDto) {
+        // 保存校验
+        ValidatorUtil.require(categoryDto.getParent(), "父id");
+        ValidatorUtil.require(categoryDto.getName(), "名称");
+        ValidatorUtil.length(categoryDto.getName(), "名称", 1, 50);
+
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.save(categoryDto);
+        responseDto.setContent(categoryDto);
+        return responseDto;
+    }
+
+    /**
+     * 删除
+     */
+    @DeleteMapping("/delete/{id}")   //访问category地址
+    public ResponseDto delete(@PathVariable String id) {
+        ResponseDto responseDto = new ResponseDto();
+        categoryService.delete(id);
+        return responseDto;
+    }
+}
---
+++ server/src/main/java/com/coursera/server/service/CategoryService.java
@@ -0,0 +1,72 @@
+package com.coursera.server.service;
+
+import com.coursera.server.util.CopyUtil;
+import com.coursera.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
+import com.coursera.server.domain.Category;
+import com.coursera.server.domain.CategoryExample;
+import com.coursera.server.dto.CategoryDto;
+import com.coursera.server.dto.PageDto;
+import com.coursera.server.mapper.CategoryMapper;
+import com.github.pagehelper.PageInfo;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    /**
+     * 列表查询
+     */
+    public void list(PageDto pageDto){
+        PageHelper.startPage(pageDto.getPage(),pageDto.getSize()); //分页功能
+        CategoryExample categoryExample = new CategoryExample();
+        categoryExample.setOrderByClause("sort asc");
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+        PageInfo<Category> pageInfo = new PageInfo<>(categoryList);
+        pageDto.setTotal(pageInfo.getTotal());
+        List<CategoryDto> categoryDtoList = CopyUtil.copyList(categoryList, CategoryDto.class);
+        pageDto.setList(categoryDtoList);
+    }
+
+    /**
+     * 保存，id有值时更新，无值时新增
+     */
+    public void save(CategoryDto categoryDto){
+        Category category = CopyUtil.copy(categoryDto, Category.class);
+        if(StringUtils.isEmpty(categoryDto.getId())){
+            this.insert(category);
+        } else {
+            this.update(category);
+        }
+    }
+
+    /**
+     * 新增
+     */
+    private void insert(Category category){
+        category.setId(UuidUtil.getShortUuid());
+        categoryMapper.insert(category);
+    }
+
+    /**
+     * 更新
+     */
+    private void update(Category category){
+        categoryMapper.updateByPrimaryKey(category);
+    }
+
+    /**
+     * 删除
+     */
+    public void delete(String id){
+        categoryMapper.deleteByPrimaryKey(id);
+
+    }
+}
