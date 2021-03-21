ccf4d3ffbdbf7bbff129b7237b5865d6717f4e90
tiareWang <a986261752@gmail.com>
2021-03-21 15:51:21 -0700

    1. 保存节时，更新课程总时长 2. 增加自定义mapper

---
+++ server/src/main/java/com/coursera/server/mapper/my/MyCourseMapper.java
@@ -0,0 +1,8 @@
+package com.coursera.server.mapper.my;
+
+import org.apache.ibatis.annotations.Param;
+
+public interface MyCourseMapper {
+
+    int updateTime(@Param("courseId") String courseId);
+}
--- server/src/main/java/com/coursera/server/service/CourseService.java
+++ server/src/main/java/com/coursera/server/service/CourseService.java
@@ -1,26 +1,34 @@
 package com.coursera.server.service;
 
-import com.coursera.server.util.CopyUtil;
-import com.coursera.server.util.UuidUtil;
-import com.github.pagehelper.PageHelper;
 import com.coursera.server.domain.Course;
 import com.coursera.server.domain.CourseExample;
 import com.coursera.server.dto.CourseDto;
 import com.coursera.server.dto.PageDto;
 import com.coursera.server.mapper.CourseMapper;
+import com.coursera.server.mapper.my.MyCourseMapper;
+import com.coursera.server.util.CopyUtil;
+import com.coursera.server.util.UuidUtil;
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
-import java.util.List;
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class CourseService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CourseService.class);
+
     @Resource
     private CourseMapper courseMapper;
+
+    @Resource
+    private MyCourseMapper myCourseMapper;
 
     /**
      * 列表查询
@@ -73,5 +81,15 @@ public class CourseService {
     public void delete(String id){
         courseMapper.deleteByPrimaryKey(id);
 
+    }
+
+    /**
+     * 更新课程时长
+     * @param courseId
+     * @return
+     */
+    public void updateTime(String courseId) {
+        LOG.info("更新课程时长：{}", courseId);
+        myCourseMapper.updateTime(courseId);
     }
 }
