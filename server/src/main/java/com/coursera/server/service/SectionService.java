a996f9e2460813decbac1024d1c8dab61a4bd2a9
tiareWang <a986261752@gmail.com>
2021-03-21 16:02:53 -0700

    1. 增加事务配置

--- server/src/main/java/com/coursera/server/service/SectionService.java
+++ server/src/main/java/com/coursera/server/service/SectionService.java
@@ -10,6 +10,7 @@ import com.coursera.server.util.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -49,6 +50,7 @@ public class SectionService {
     /**
      * 保存，id有值时更新，无值时新增
      */
+    @Transactional
     public void save(SectionDto sectionDto){
         Section section = CopyUtil.copy(sectionDto, Section.class);
         if(StringUtils.isEmpty(sectionDto.getId())){
@@ -58,6 +60,9 @@ public class SectionService {
         }
         courseService.updateTime(sectionDto.getCourseId());
     }
+    //事务不起作用：exception（除非加rollbackFor=Exception.class)，但runtimeException可以起作用，回滚，
+    // 同一个类的内部方法互相调用，methodA调用methodB，methodB事务不起作用
+    //Spring的事务处理是利用AOP生成动态代理类，内部方法调用时不经过代理类，所以事务不生效
 
     /**
      * 新增
