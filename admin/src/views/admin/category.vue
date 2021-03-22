2fc44686ee5cdb031543083ad077b620fc07543b
tiareWang <a986261752@gmail.com>
2021-03-21 20:05:30 -0700

    1. 两级分类的新增修改删除

--- admin/src/views/admin/category.vue
+++ admin/src/views/admin/category.vue
@@ -3,9 +3,9 @@
     <div class="row">
       <div class="col-md-6">
         <p>
-          <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+          <button v-on:click="add1()" class="btn btn-white btn-default btn-round">
             <i class="ace-icon fa fa-edit"></i>
-            新增
+            新增一级
           </button>
           &nbsp;
           <!--代表空格-->
@@ -44,9 +44,9 @@
       </div>
       <div class="col-md-6">
         <p>
-          <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+          <button v-on:click="add2()" class="btn btn-white btn-default btn-round">
             <i class="ace-icon fa fa-edit"></i>
-            新增
+            新增二级
           </button>
         </p>
 
@@ -90,9 +90,9 @@
           <div class="modal-body">
             <form class="form-horizontal">
               <div class="form-group">
-                <label class="col-sm-2 control-label">父id</label>
+                <label class="col-sm-2 control-label">父分类</label>
                 <div class="col-sm-10">
-                  <input v-model="category.parent" class="form-control">
+                  <p class="form-control-static">{{active.name || "无"}}</p>
                 </div>
               </div>
               <div class="form-group">
@@ -139,13 +139,31 @@
     },
     methods: {
       /**
-       * 点击【新增】
+       * 点击【新增一级】
        */
-      add() {
+      add1() {
         let _this = this;
-        _this.category = {};
+        _this.active = {};
+        _this.level2 = [];
+        _this.category = {
+          parent: "00000000"
+        };
         $("#form-modal").modal("show");
-      //  $(".modal").modal({backdrop:"static"});禁止点空白地方关闭，某些场景会用到这个功能
+      },
+
+      /**
+       * 点击【新增二级】
+       */
+      add2() {
+        let _this = this;
+        if (Tool.isEmpty(_this.active)) {
+          Toast.warning("请先点击一级分类");
+          return;
+        }
+        _this.category = {
+          parent: _this.active.id
+        };
+        $(".modal").modal("show");
       },
 
       /**
--- server/src/main/java/com/coursera/server/service/CategoryService.java
+++ server/src/main/java/com/coursera/server/service/CategoryService.java
@@ -10,6 +10,7 @@ import com.coursera.server.dto.PageDto;
 import com.coursera.server.mapper.CategoryMapper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -76,8 +77,24 @@ public class CategoryService {
     /**
      * 删除
      */
+    @Transactional
     public void delete(String id){
+        deleteChildren(id);
         categoryMapper.deleteByPrimaryKey(id);
-
     }
+
+    /**
+     * 删除子分类
+     * @param id
+     */
+    public void deleteChildren(String id){
+        Category category = categoryMapper.selectByPrimaryKey(id);
+        if ("00000000".equals(category.getParent())) {
+            // 如果是一级分类，需要删除其下的二级分类
+            CategoryExample example = new CategoryExample();
+            example.createCriteria().andParentEqualTo(category.getId());
+            categoryMapper.deleteByExample(example);
+        }
+    }
+
 }
